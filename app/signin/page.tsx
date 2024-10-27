9740ce8f41cce330faf0be8373f1b7fc104b079d
wakisakamac <naopy702@gmail.com>
2024-10-27 21:36:32 +0900

    ボタンが動くようになった

--- app/signin/page.tsx
+++ app/signin/page.tsx
@@ -1,21 +1,18 @@
-
+'use Client'
 
 import { LoginButton, LogoutButton, SignupButton } from "../components/ui_parts/Buttons";
 import { SigninTitle } from "../components/ui_parts/SigninTitle";
-//import styles from "../../app/styles/signin.module.css"
 import UserIcon from "../components/ui_parts/UserIcon";
 import UserStatus from "../components/ui_parts/UserStatus";
 import getCurrentUser from '@/app/actions/getCurrentUser'
 import styles from "@/app/statics/styles/signin.module.css";
 
 
-
 export default async function SignIn() {
     const currentUser = await getCurrentUser();
-    console.log("あああああああああああああああああ");
     return (
-        <div className={styles.bg}>
-            <div className = {styles.frame}>
+        <>
+            <div className={styles.bg}>
                 <div className={styles.body}>
                     <div className={styles.user}>
                         <UserStatus/>
@@ -34,6 +31,6 @@ export default async function SignIn() {
                     )}
                 </div>
             </div>
-        </div>
+        </>
     )
 }
