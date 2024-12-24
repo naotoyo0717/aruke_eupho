be17f11c14c20e3b955262c21258e23ed7c20ba6
naotoyo0717 <saekiharuko17@gmail.com>
2024-12-24 18:03:49 +0900

    getOrigin関数を作成

--- app/map/page.tsx
+++ app/map/page.tsx
@@ -19,6 +19,26 @@ const Page = () => {
 
   const startingPointInt = Number(startingPoint);
 
+
+  function getOrigin(startingPointInt: number) {
+    switch (startingPointInt) {
+      case 1:
+        return { name: '京阪宇治駅', lat: 34.89483083112986, lng: 135.8070045145702 };
+      case 2:
+        return { name: '京阪黄檗駅', lat: 34.91433429093788, lng: 135.80257257542314 };
+      case 3:
+        return { name: '京阪六地蔵駅', lat: 34.93206069950424, lng: 135.79328135941512 };
+      case 4:
+        return { name: 'JR宇治駅', lat: 34.89047759941201, lng: 135.8007251883931 };
+      case 5:
+        return { name: 'JR京都駅', lat: 34.98589834797202, lng: 135.75885200990197 };
+      default:
+        console.error('不明な出発地点です');
+        return null;
+    }
+  }
+  
+
   useEffect(() => {
     const fetchSelectedLocation = async () => {
       try {
@@ -50,28 +70,11 @@ const Page = () => {
       );
     } else {
       // 他の出発地点に対応
-      switch (startingPointInt) {
-        case 1:
-          setOrigin({ name: '京阪宇治駅', lat: 34.89483083112986, lng: 135.8070045145702 });
-          break;
-        case 2:
-          setOrigin({ name: '京阪黄檗駅', lat: 34.91433429093788, lng: 135.80257257542314 });
-          break;
-        case 3:
-          setOrigin({ name: '京阪六地蔵駅', lat: 34.93206069950424, lng: 135.79328135941512 });
-          break;
-        case 4:
-          setOrigin({ name: 'JR宇治駅', lat: 34.89047759941201, lng: 135.8007251883931 });
-          break;
-        case 5:
-          setOrigin({ name: 'JR京都駅', lat: 34.98589834797202, lng: 135.75885200990197 });
-          break;
-        default:
-          console.error('不明な出発地点です');
-          break;
+      const originData = getOrigin(startingPointInt);
+      if (originData) {
+        setOrigin(originData);
       }
     }
-
   }, [startingPointInt]);
 
   const waypoints: WayPoint[] = selectedWayPoints.map((point) => ({
@@ -104,12 +107,6 @@ const Page = () => {
           duration={duration}
           selectedWayPoints={selectedWayPoints}
         />
-        {/* <MapSideBar
-          origin={origin.name}
-          duration={duration}
-          selectedWayPoints={selectedWayPoints}
-          order={orderRef}
-        /> */}
       </div>
     )
   );
