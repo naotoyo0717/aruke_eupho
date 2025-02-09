72ad72fdf00feef9a08212be7b40ddf2c65a4869
naotoyo0717 <saekiharuko17@gmail.com>
2025-02-09 23:16:18 +0900

    ビルド完了

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,70 +1,7 @@
-import NextAuth, { NextAuthOptions } from 'next-auth'
-import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import NextAuth from 'next-auth'
+import { authOptions } from '@/app/lib/nextAuth'
 
-import CredentialsProvider from 'next-auth/providers/credentials'
-import GoogleProvider from 'next-auth/providers/google'
-import bcrypt from 'bcrypt'
-import prisma from '@/app/lib/prisma'
-//import prisma from '@prisma/client'
-//import { DefaultArgs } from '@prisma/client/runtime/library'
-
-// NextAuth設定
-export const authOptions: NextAuthOptions = {
-    //　Prismaを使うための設定
-    adapter: PrismaAdapter(prisma),
-    //  認証プロバイダーの設定
-    providers: [
-        // Google認証
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-        }),
-
-        // メールアドレス認証
-        CredentialsProvider({
-            name: 'credentials',
-            credentials: {
-            // メールアドレスとパスワード
-            email: { label: 'email', type: 'text' },
-            password: { label: 'password', type: 'password' },
-        },
-
-        async authorize(credentials) {
-            // メールアドレスとパスワードがない場合エラー
-            if (!credentials?.email || !credentials?.password) {
-                throw new Error('メールアドレスとパスワードが存在しません')
-            }
-
-            // ユーザーを取得
-            const user = await prisma.user.findUnique({
-                where: {
-                    email: credentials.email,
-                }
-            })
-
-            // ユーザーが存在しない場合はエラー
-            if (!user || !user?.hashedPassword) {
-                throw new Error('ユーザーが存在しません')
-            }
-
-            // パスワードが存在しない場合はエラー
-            const isCorrectPassword = await bcrypt.compare(credentials.password, user.hashedPassword)
-
-            if (!isCorrectPassword) {
-                throw new Error('パスワードが一致しません')
-            }
-
-            return user
-            },
-        }),
-    ],
-    session: {
-        strategy: 'jwt',
-    },
-    secret: process.env.NEXTAUTH_SECRET,
-}
-
+// NextAuthのハンドラーをエクスポートする
 const handler = NextAuth(authOptions)
 
 export { handler as GET, handler as POST }
-
