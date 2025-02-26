dcd7f94b43c2977b96fffb7f08ba35582bca9461
naotoyo0717 <saekiharuko17@gmail.com>
2025-02-26 12:40:02 +0900

    barButtonをsxに移動

--- app/components/map_sidebar/MapSideBar.tsx
+++ app/components/map_sidebar/MapSideBar.tsx
@@ -54,7 +54,22 @@ export default function MapSideBar({ origin, duration, selectedWayPoints, order 
     return (
         <div>
             {!show && (
-                <Button className={styles.barButton} onClick={openDrew}>
+                <Button 
+                //className={styles.barButton}
+                    onClick={openDrew}
+                    sx={{
+                        position: 'absolute',
+                        top: 0,
+                        right: '10px',
+                        zIndex: 1000, // キャメルケースに修正
+                        backgroundColor: '#FFF', // キャメルケースに修正
+                        '@media (max-width: 600px)': {
+                            paddingRight: '8vw', // キャメルケースに修正
+                            right: '0px', // 数値は文字列で統一
+                            transform: 'translateX(30%)', // 文字列として記述
+                        },
+                    }}                    
+                >
                     <KeyboardDoubleArrowLeftIcon
                         sx={{
                             fontSize: 60,
